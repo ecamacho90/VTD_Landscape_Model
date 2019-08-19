ad2ca33ba775f76feddc736f7465406224951bbe
meritxellsaez <50885002+meritxellsaez@users.noreply.github.com>
2019-08-19 11:52:42 +0100

    Add files via upload

--- Landscapes/MCMC_Exploration/VTD_Landscape_Model_v1_Priors.m
+++ Landscapes/MCMC_Exploration/VTD_Landscape_Model_v1_Priors.m
@@ -4,7 +4,7 @@ if paramnumber==1
     %a
     if CaseLandscape == 1
         %pvalue = -random('Gamma',10,0.9);
-        pvalue = -random('Gamma',10,1.5);%% 80% -> 40% with height saddles
+        pvalue = -random('Gamma',12,1.1);%% 50% with height saddles
     elseif CaseLandscape == 2
         pvalue = -random('Gamma',10,0.9);
     elseif CaseLandscape == 3
@@ -43,7 +43,7 @@ elseif paramnumber==4
     %u
     if CaseLandscape == 1
         %pvalue = -random('Uniform',0,50);
-        pvalue = -random('Gamma',3,6);
+        pvalue = -random('Gamma',3.7,6);
     elseif CaseLandscape == 2
         pvalue = -random('Uniform',0,50);
     elseif CaseLandscape == 3
@@ -59,7 +59,7 @@ elseif paramnumber==5
     %v
     if CaseLandscape == 1
        %pvalue = random('Uniform',0,50);%%12%
-        pvalue = random('Gamma',2.5,5);
+        pvalue = random('Gamma',3.5,4.5);
     elseif CaseLandscape == 2
         pvalue = random('Uniform',0,50);
     elseif CaseLandscape == 3
